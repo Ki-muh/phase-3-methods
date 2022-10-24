c284999001dd48973acaf72938d7048ccfff9297
Martin Kamau <martinkimu09@gmail.com>
2022-10-24 09:01:15 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet name="Naureen"
+    puts "Hello, #{name}!"
+end
+
+greet
+
+greet "Jimmy"
+
+def greet_with_default name="programmer"
+    puts "Hello, #{name}!"
+end
+
+greet_with_default 
+
+greet_with_default "Sunny"
+
+def add num1=2, num2=5
+    return num1 + num2
+end
+
+puts add
+
+def halve num3=6, num4=2
+    half=num3/num4
+    return half 
+    return nil if num4!=integer
+end
+
+puts halve
